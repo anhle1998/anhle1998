71803cbd9135e543910243181b825786f62f0184
anhle1998 <ldong144@gmail.com>
2022-03-23 22:41:48 +0700

    first commit

---
+++ b1.js
@@ -0,0 +1,11 @@
+let arr = [0, 5, 4, 2, 8];
+
+function sumArray(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+ 
+console.log(sumArray(arr));
---
+++ b2.js
@@ -0,0 +1,11 @@
+const person = [
+	{ name: 'John', age: 24 },
+	{ name: 'Pete', age: 25 },
+	{ name: 'Mary', age: 28 }];
+
+    const arrname = [];
+    for (var i = 0; i < person.length;i++){
+        arrname.push(person[i].name) ;
+      
+    }
+    console.log(arrname)
